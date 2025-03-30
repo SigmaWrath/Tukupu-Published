cfd21bb9ff2621b53772d673bd0a4d75c234118d
Sumrath Kaur <sumrath.pahwa@gmail.com>
2025-03-29 20:00:14 -0700

    Revert "fix(alias): resolve relative if alias is relative"
    This reverts commit c18e6cd5bb7e0de7e1a9a77927b48069f0bad4b9.

--- quartz/plugins/emitters/aliases.ts
+++ quartz/plugins/emitters/aliases.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { resolveRelative, simplifySlug } from "../../util/path"
-=======
-import { isRelativeURL, resolveRelative, simplifySlug } from "../../util/path"
->>>>>>> parent of 23b691f (fix: coerce fullslug)
 import { QuartzEmitterPlugin } from "../types"
 import { write } from "./helpers"
 import { BuildCtx } from "../../util/ctx"
@@ -11,17 +7,8 @@ import { VFile } from "vfile"
 async function* processFile(ctx: BuildCtx, file: VFile) {
   const ogSlug = simplifySlug(file.data.slug!)
 
-<<<<<<< HEAD
   for (const slug of file.data.aliases ?? []) {
     const redirUrl = resolveRelative(slug, file.data.slug!)
-=======
-  for (const aliasTarget of file.data.aliases ?? []) {
-    const aliasTargetSlug = isRelativeURL(aliasTarget)
-      ? path.normalize(path.join(ogSlug, "..", aliasTarget))
-      : aliasTarget
-
-    const redirUrl = resolveRelative(aliasTargetSlug, ogSlug)
->>>>>>> parent of 23b691f (fix: coerce fullslug)
     yield write({
       ctx,
       content: `
